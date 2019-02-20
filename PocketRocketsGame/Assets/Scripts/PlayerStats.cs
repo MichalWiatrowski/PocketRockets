e4de1217d733a6852b4b36c6767d1e5e33d95ec9
Ruzry <35919602+Ruzry@users.noreply.github.com>
2019-02-20 16:11:12 +0000

    Player stats update
    Updated player stats to have Getter and Setters. Not implemented elsewhere yet, waiting for merge.

--- PocketRocketsGame/Assets/Scripts/PlayerStats.cs
+++ PocketRocketsGame/Assets/Scripts/PlayerStats.cs
@@ -9,46 +9,75 @@ public class PlayerStats : MonoBehaviour {
 
     // player stat variables
     public string playerName;
+
     public float speed = 4f;
     public float defaultSpeed = 30.0f;
     public float slowDownFactor = 1.0f;
-    public bool immune = false;
+
     public int points = 600;
     public int position = 1;
     public int nextGate = 1;
-    public bool switchLeft = true;
-    public bool switchRight = true;
-  
 
-    //Getters and Seer done
     public bool winner = false;
     public bool fallingThroughTeleport = false;
     public bool trappedInBubble = false;
     public bool inTheAir = false;
     public bool jumping = false;
+    public bool switchLeft = true;
+    public bool switchRight = true;
+    public bool immune = false;
 
     void Start()
     {
         //defaultSpeed = speed;
     }
-
     
+    //Falling Through Teleport
     public bool getFallingThroughTeleport(){ return fallingThroughTeleport;}
     public void setFallingThroughTeleport(bool flag) { fallingThroughTeleport = flag;}
+    //In The Air
     public bool getInTheAir(){ return inTheAir;}
     public void setInTheAir(bool flag) { inTheAir = flag;}
+    //Trapped in Bubble
     public bool getTrappedInBubble() { return trappedInBubble; }
     public void setTrappedInBubble(bool flag) { trappedInBubble = flag; }
+    //Is Winner
     public bool isWinner() { return winner; }
     public void getIsWinner(bool flag) { winner = flag; }
+    //Jumping
     public bool getJumping() { return jumping; }
     public void setJumping(bool flag) { jumping = flag; }
+    //Switch Left
+    public bool getSwitchLeft() { return switchLeft; }
+    public void setSwitchLeft(bool flag) { switchLeft = flag; }
+    //Switch Right
+    public bool getSwitchRight() { return switchRight; }
+    public void setSwitchRight(bool flag) { switchRight = flag; }
+    //Immune
+    public bool getImmune() { return immune; }
+    public void setImmune(bool flag) { immune = flag; }
+    //Default Speed
+    public float getDefaultSpeed() { return defaultSpeed; }
+    public void setDefaultSpeed(float spd) { defaultSpeed = spd; }
+    //Slow down Factor
+    public float getSlowDownFactor() { return slowDownFactor; }
+    public void setSlowDownFactor(float slowDown){ slowDownFactor = slowDown; }
+    //Name
+    public string getPlayerName() {  return playerName; }
+    public void setPlayerName(string name) { playerName = name; }
+    //Points
+    public int getPoints() { return points; }
+    public void setPoints(int pointValue) { points = pointValue; }
+    //Position
+    public int getPosition() { return position; }
+    public void setPosition(int positionValue) { position = positionValue; }
+    //Next Gate
+    public int getNextGate() { return nextGate; }
+    public void setNextGate(int gateNum) { nextGate = gateNum; }
+    //Current Lane
+    public int getCurrentLane() { return currentLane; }
+    public void setCurrentLane(int currLane) { currentLane = currLane; }
 
-
-    public void setSlowDownFactor(float slowDown)
-    {
-        slowDownFactor = slowDown;
-    }
 
     private void Update()
     {
@@ -63,7 +92,7 @@ public class PlayerStats : MonoBehaviour {
 
 
             inTheAir = false;
-           GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().useGravity = true;
             GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
 
             jumping = false;
@@ -79,6 +108,10 @@ public class PlayerStats : MonoBehaviour {
         }
 
     }
+
+    //Speed
+    public float getSpeed() { return speed; }
+    public void setSpeed(float spd) { speed = spd; }
 
     public void incrementSpeed(float incrementValue)
     {
